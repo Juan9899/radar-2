a64e0a07f3ca5d162a91b1a3d911628a28a8d1c3
Juan9899 <98743364+Juan9899@users.noreply.github.com>
2022-05-25 09:01:03 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 radio.onReceivedString(function (receivedString) {
-    signal = Math.map(-95, -42, 0, 9, 9)
+    signal = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    led.plotBarGraph(
+    Math.map(signal, -95, -42, 0, 9),
+    9
+    )
 })
 let signal = 0
 radio.setGroup(53)
